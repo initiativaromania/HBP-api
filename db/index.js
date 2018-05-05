48a3892543ed4fcb329d8afdce382b90b5660d73
George <georgeserbanescu@hotmail.com>
2018-05-05 14:34:46 +0300

    Update config for PG

--- db/index.js
+++ db/index.js
@@ -1,7 +1,16 @@
-const pg = require('pg')
-require('pg-spice').patch(pg)
+const pg = require('pg');
 
-const pool = new pg.Pool()
+const config = {
+  host: process.env.CUSTOMCONNSTR_PGHOST,
+  user: process.env.CUSTOMCONNSTR_PGUSER,     
+  password: process.env.CUSTOMCONNSTR_PGPASSWORD,
+  database: process.env.CUSTOMCONNSTR_PGDATABASE,
+  port: process.env.CUSTOMCONNSTR_PGPORT
+};
+
+require('pg-spice').patch(pg);
+
+const pool = new pg.Pool(config);
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
