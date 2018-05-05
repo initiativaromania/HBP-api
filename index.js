281721be48fc27be1e102d397fae2feb7ff85c10
George <georgeserbanescu@hotmail.com>
2018-05-05 13:01:07 +0300

    Compile es6 with babel as the vesion of node from azure doesn't like it

--- index.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app')
-const env = require('dotenv').config();
-
-var port = process.env.PORT || 8081;
-app.listen(port, () => { console.log(`server is listening on ${port}`) })
