17a68d70bd5e9928fd6781e75816d7d1f9524b6c
George Serbanescu <georgeserbanescu@hotmail.com>
2018-05-07 13:08:56 +0300

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,17 +1,18 @@
 const pg = require('pg');
 const env = require('dotenv').config();
 
-const config = {
-  host: process.env.CUSTOMCONNSTR_PGHOST,
-  user: process.env.CUSTOMCONNSTR_PGUSER,     
-  password: process.env.CUSTOMCONNSTR_PGPASSWORD,
-  database: process.env.CUSTOMCONNSTR_PGDATABASE,
-  port: process.env.CUSTOMCONNSTR_PGPORT
-};
+// const config = {
+//   host: process.env.CUSTOMCONNSTR_PGHOST,
+//   user: process.env.CUSTOMCONNSTR_PGUSER,     
+//   password: process.env.CUSTOMCONNSTR_PGPASSWORD,
+//   database: process.env.CUSTOMCONNSTR_PGDATABASE,
+//   port: process.env.CUSTOMCONNSTR_PGPORT
+// };
 
 require('pg-spice').patch(pg);
 
-const pool = new pg.Pool(config);
+//const pool = new pg.Pool(config);
+const pool = new pg.Pool();
 
 module.exports = {
   query: (text, params) => pool.query(text, params)
